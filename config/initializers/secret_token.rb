56465e817389c18d2d62b479601562ac5831ccbd
Auzre <azure@AzurePC.(none)>
2014-06-15 21:32:35 +0800

    初次提交

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '24103c71af2fc86dd44c43a89748667b6f26178f2204bfa27f81d41d646f1f1c69f8ae90d0f5d4b1e6c29c9cbe323a2ac3b09c7ce3440259343eefac90714460'
